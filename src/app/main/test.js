4a5991748df2b0f0dc98c463699849a9131c2565
PawelKaniak <pawkaniak@gmail.com>
2017-11-21 20:15:39 +0100

    simple react-router configuration

---
+++ src/app/main/test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Test extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>
+                    pathTest
+                </h1>
+            </div>
+        );
+    };
+}
+
+export default Test;
